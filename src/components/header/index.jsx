7a9b004250e94ffd90a101cdf25cc80db7c58edc
Jair Barreto <jair@gginformaticaweb.com>
2023-05-07 14:54:32 -0300

    feat: add user reducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 
 // Components
 import Cart from "../cart/index";
@@ -6,24 +7,44 @@ import Cart from "../cart/index";
 // Styles
 import * as Styles from "./styles";
 
+import { loginUser, logoutUser } from '../../redux/user/actions';
+
 function Header() {
-  const [cartIsVisible, setCartIsVisible] = useState(false);
+    const [cartIsVisible, setCartIsVisible] = useState(false);
 
-  const handleCartClick = () => {
+    const { currentUser } = useSelector( rootReducer => rootReducer.userReducer );
+    const dispatch = useDispatch();
+
+    console.log({currentUser})
+
+    const handleLoginClick = () => {
+        dispatch(loginUser({ name:'Jair Barreto', email: 'jair@barreto.com' }));
+    };
+
+    const handleLogoutClick = () => {
+        dispatch(logoutUser())
+    }
+
+    const handleCartClick = () => {
     setCartIsVisible(true);
-  };
+    };
 
-  return (
-    <Styles.Container>
-      <Styles.Logo>Redux Shopping</Styles.Logo>
-      <Styles.Buttons>
-        <div>Login</div>
-        <div onClick={handleCartClick}>Carrinho</div>
-      </Styles.Buttons>
+    return (
+        <Styles.Container>
+            <Styles.Logo>Redux Shopping</Styles.Logo>
+            <Styles.Buttons>
+                {
+                    currentUser ? 
+                    ( <div onClick={handleLogoutClick}>Logout</div> )
+                    :
+                    ( <div onClick={handleLoginClick}>Login</div> )
+                }
+                <div onClick={handleCartClick}>Carrinho</div>
+            </Styles.Buttons>
 
-      <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
-    </Styles.Container>
-  );
+            <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
+        </Styles.Container>
+    );
 }
 
 export default Header;
